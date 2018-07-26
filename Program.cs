8d95a179038ba71c5318f0ce6d23bfd5f38ecb45
Epiger <wigo118@hotmail.de>
2018-07-26 12:16:42 +0200

    This initial Commit creates a useless Eto Window with .NET 4.7.2

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sizer
+{
+    public class Program{
+		[STAThread]
+		public static void Main(string[] args){
+			Console.WriteLine("TEst");
+
+
+			new Eto.Forms.Application().Run(new MyForm());
+
+		}
+	}
+
+
+	class MyForm : Eto.Forms.Form
+{
+	public MyForm()
+	{
+		// sets the client (inner) size of the window for your content
+		this.ClientSize = new Eto.Drawing.Size(600, 400);
+
+		this.Title = "Hello, Eto.Forms";
+	}
+}
+
+}
