e7b4b6e1a1cda40758203603dc53ab5e44edf3c8
Epiger <wigo118@hotmail.de>
2018-08-01 02:14:11 +0200

    copied the tut

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Eto.Forms;
 
 namespace Sizer
 {
@@ -14,15 +15,52 @@ namespace Sizer
 	}
 
 
-	class MyForm : Eto.Forms.Form
-{
-	public MyForm()
-	{
-		// sets the client (inner) size of the window for your content
-		this.ClientSize = new Eto.Drawing.Size(600, 400);
+	class MyForm : Eto.Forms.Form {
+		public MyForm() {
+			// sets the client (inner) size of the window for your content
+			this.ClientSize = new Eto.Drawing.Size(600, 400);
 
-		this.Title = "Hello, Eto.Forms";
+			this.Title = "Hello, Eto.Forms";
+
+			Menu = new MenuBar{
+				Items = {
+					new ButtonMenuItem{
+						Text = "&File",
+						Items = {
+							new MCommand(),
+
+							new ButtonMenuItem {
+								Text = "Click me button"
+							}
+						}
+					}
+
+				}
+
+
+			};
+
+		}
 	}
-}
+
+
+	class MCommand : Command {
+
+		public MCommand(){
+			MenuText = "Click Me!!";
+			ToolBarText = "HEHEH Toolbar click";
+			ToolTip = "TToool tipp?";
+			
+			Shortcut = Application.Instance.CommonModifier | Keys.M;
+		}
+
+		protected override void OnExecuted(EventArgs e){
+			base.OnExecuted(e);
+
+			MessageBox.Show(Application.Instance.MainForm, "Clicked hahaha!!", "Tutle", MessageBoxType.Information);
+		}
+
+
+	}
 
 }
